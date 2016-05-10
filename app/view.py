534989d2e6729db4d016dee11e0525d47c535672
Deetrixx <dennisgithua30@gmail.com>
2016-05-10 18:55:19 +0300

    added a base file,about file and a app route for about file

--- app/view.py
+++ app/view.py
@@ -23,3 +23,24 @@ def index():
                             user=user
                             posts=posts)
 
+@app.route('/')
+@app.route('/about')
+
+def about():
+    user={'nickname':''} #fake user
+    posts =[ #fake array of posts
+        {
+            'author':{'nickname':'Joan'},
+            'body':'Beautiful day in Portland!'
+        },
+        {
+            'author':{'nickname':'Suan'},
+            'body':'The Avengers movie was so cool!'
+         }
+     ]
+
+    return render_template('about.html',
+                            title='About',
+                            user=user,
+                            posts=posts)
+
