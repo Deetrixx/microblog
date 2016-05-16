3d4222de633bbc1e3a60de0c44aed8fcf75cb46a
Deetrixx <dennisgithua30@gmail.com>
2016-05-16 05:25:50 +0300

    initialized my database

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-from app import view
+app.config.from_object('config')
+db = SQLALchemy(app)
+
+from app import view, models
